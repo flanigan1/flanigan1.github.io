047a9d6b767c8a1cb22902c55cc312715aa87d32
flanigan1 <flanb366@newschool.edu>
2017-04-20 17:58:54 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+console.log("linked")
+
+var hello="hello there";
+console.log(hello);
+
+var one=1;
+var two=2
+console.log(one+two);
+
+if (1>0){
+
+	alert("aren't pop ups irritating?")    
+
+}                                                                                      
+
+var CB = document.getElementById("BUTTON2");
+
+CB.addEventListener('click', click_bait);
+
+function click_bait() {
+	alert("I told you not to click me.");
+}
+
+
+function pop_up() {
+	document.getElementById('button').innerHTML='JavaScript is neat.'
+	document.getElementById('button').style.color="red"
+}
+
